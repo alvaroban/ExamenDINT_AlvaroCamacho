03492d13c3c8f61396510302f71a4e538c3d2b57
Alvaro Camacho <accc1997@hotmail.com>
2025-03-11 19:23:10 +0100

    Commit final

---
+++ ExamenDINT_AlvaroCamacho/view/MenView.swift
@@ -0,0 +1,18 @@
+//
+//  MenView.swift
+//  ExamenDINT_AlvaroCamacho
+//
+//  Created by Alvaro Camacho on 11/3/25.
+//
+
+import SwiftUI
+
+struct MenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenView()
+}
